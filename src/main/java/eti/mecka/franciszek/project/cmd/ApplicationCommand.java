18cc3aa131bb495b4b650f4d839739cdf8b34d3f
FranciszekMecka <franciszek.mecka@gmail.com>
2023-11-03 14:31:19 +0100

    Improve CommandLineRunner

--- src/main/java/eti/mecka/franciszek/project/cmd/ApplicationCommand.java
+++ src/main/java/eti/mecka/franciszek/project/cmd/ApplicationCommand.java
@@ -4,7 +4,10 @@ import eti.mecka.franciszek.project.player.entity.Organization;
 import eti.mecka.franciszek.project.player.entity.Player;
 import eti.mecka.franciszek.project.player.service.api.OrganizationService;
 import eti.mecka.franciszek.project.player.service.api.PlayerService;
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -12,7 +15,6 @@ import java.util.*;
 
 @Component
 public class ApplicationCommand implements CommandLineRunner {
-
 
     private final PlayerService playerService;
     private final OrganizationService organizationService;
@@ -26,14 +28,27 @@ public class ApplicationCommand implements CommandLineRunner {
     public void run(String... args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         String command;
-
+        System.out.println("Type in 'help' to see the command list.");
         main_loop: while (true) {
+
             command = scanner.next();
             switch (command) {
+                case "help" -> {
+                    System.out.println("""
+                            Available commands:
+                            'get_players' - prints out all the players
+                            'get_organizations' - prints out all organizations
+                            'get_organization_players' - prints out all players from given organization
+                            'get_player' - prints out a specific player that matches UUID from the user input
+                            'delete_player' - deletes a player that has given UUID
+                            'put_player' - creates a player from user input
+                            'quit' - closes the application""");
+                }
                 case "get_players" -> {
                     playerService.findAll().forEach(System.out::println);
                 }
                 case "get_organization_players" -> {
+                    System.out.println("Input Organization's UUID:");
                     UUID uuid = UUID.fromString(scanner.next());
                     try {
                         playerService.findAllByOrganization(uuid).ifPresent(System.out::println);
@@ -42,15 +57,17 @@ public class ApplicationCommand implements CommandLineRunner {
                     }
                 }
                 case "get_player" -> {
+                    System.out.println("Input UUID of the player:");
                     UUID uuid = UUID.fromString(scanner.next());
                     try {
-                        playerService.find(uuid);
+                        playerService.find(uuid).ifPresent(System.out::println);
                     } catch (NoSuchElementException ex){
                         System.out.println("NOT_FOUND");
                     }
                 }
                 case "delete_player" -> {
                     try {
+                        System.out.println("Input UUID of the player that you want to delete:");
                         UUID uuid = UUID.fromString(scanner.next());
                         playerService.delete(uuid);
                     } catch (NoSuchElementException ex){
@@ -59,11 +76,19 @@ public class ApplicationCommand implements CommandLineRunner {
                 }
                 case "put_player" -> {
                     try {
-
                         System.out.println("Choose one of the organizations, type in the UUID of the desired one.");
                         organizationService.findAll().forEach(System.out::println);
                         Optional<Organization> organization = organizationService.find(UUID.fromString(scanner.next()));
-
+                        System.out.println("Input arguments in order:\n" +
+                                " id\n" +
+                                "first_name\n" +
+                                "surname\n" +
+                                "nationality\n" +
+                                "jerseyNumber\n" +
+                                "age\n" +
+                                "height\n" +
+                                "weight\n" +
+                                "dateOfBirth (yyyy-mm-dd");
                         Player player = Player.builder()
                                 .id(UUID.fromString(scanner.next()))
                                 .first_name(scanner.next())
@@ -74,6 +99,7 @@ public class ApplicationCommand implements CommandLineRunner {
                                 .height(scanner.nextInt())
                                 .weight(scanner.nextFloat())
                                 .dateOfBirth(LocalDate.parse(scanner.next()))
+//                                .dateOfBirth(LocalDate.of(2000, 12, 31))
                                 .organization(organization.get())
                                 .build();
                         playerService.create(player);
@@ -85,9 +111,12 @@ public class ApplicationCommand implements CommandLineRunner {
                     organizationService.findAll().forEach(System.out::println);
                 }
                 case "quit" -> {
+                    System.out.println("Closing application");
+                    System.exit(0);
                     break main_loop;
                 }
             }
         }
     }
 }
+
